a28223b14b6261102cea3ba7182ad87cd48d53c3
therickwillis <therickwillis@gmail.com>
2025-04-11 23:06:48 -0500

    click-trainer start

---
+++ click-trainer/main.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"html/template"
+	"log"
+	"math/rand"
+	"net/http"
+	"sync"
+
+	"github.com/google/uuid"
+)
+
+const (
+	gameWidth  = 600
+	gameHeight = 400
+	targetSize = 50
+)
+
+type Player struct {
+	Name  string
+	Score int
+}
+
+type Target struct {
+	X int
+	Y int
+}
+
+type GameData struct {
+	Players []*Player
+	Target  Target
+}
+
+var (
+	players   = make(map[string]*Player)
+	playersMu sync.Mutex
+
+	currentTarget Target
+	targetMu      sync.Mutex
+
+	tmpl *template.Template
+)
+
+func main() {
+	targetMu.Lock()
+	currentTarget = Target{
+		X: rand.Intn(gameWidth - targetSize),
+		Y: rand.Intn(gameHeight - targetSize),
+	}
+	targetMu.Unlock()
+
+	tmpl = template.Must(template.ParseFiles("templates/index.html", "templates/game.html"))
+
+	http.HandleFunc("/", handleIndex)
+	http.HandleFunc("/register", handleRegister)
+
+	fmt.Println("Server Listening on http://localhost:8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func handleIndex(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("[Handle:Index] Request Received")
+	if err := tmpl.Execute(w, nil); err != nil {
+		fmt.Println(err.Error())
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func handleRegister(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("[Handle:Register] Request Received")
+	if err := r.ParseForm(); err != nil {
+		fmt.Println(err.Error())
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	id := uuid.New().String()
+	name := r.FormValue("name")
+	fmt.Println("Registering name:", name)
+
+	http.SetCookie(w, &http.Cookie{
+		Name:  "player_id",
+		Value: id,
+		Path:  "/",
+	})
+
+	playersMu.Lock()
+	players[id] = &Player{Name: name}
+	playersMu.Unlock()
+
+	gameData := getGameData()
+	fmt.Println(gameData)
+	if err := tmpl.ExecuteTemplate(w, "game", gameData); err != nil {
+		fmt.Println(err.Error())
+	}
+
+	var buf bytes.Buffer
+	buf.WriteString(`<div id="join" hx-swap-oob="true" style="display:none"></div>`)
+	w.Header().Set("Content-Type", "text/html")
+	if _, err := w.Write(buf.Bytes()); err != nil {
+		fmt.Println(err.Error())
+	}
+}
+
+func getGameData() GameData {
+	playersMu.Lock()
+	defer playersMu.Unlock()
+	targetMu.Lock()
+	defer targetMu.Unlock()
+
+	playerList := make([]*Player, 0, len(players))
+	for _, p := range players {
+		playerList = append(playerList, p)
+	}
+	return GameData{
+		Players: playerList,
+		Target:  currentTarget,
+	}
+}
