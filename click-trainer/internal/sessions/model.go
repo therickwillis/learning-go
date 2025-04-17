0b9d5362af5ddd4691c230170f1b6db92ee7a2c1
therickwillis <therickwillis@gmail.com>
2025-04-17 16:35:10 -0500

    click-trainer refactor WIP

--- click-trainer/internal/sessions/model.go
+++ click-trainer/internal/sessions/model.go
@@ -1,11 +1,36 @@
 package sessions
 
-import (
-	"clicktrainer/internal/players"
-	"clicktrainer/internal/targets"
-)
+import "sync"
+
+type Player struct {
+	ID    string
+	Name  string
+	Color string
+	Score int
+}
+
+type Target struct {
+	ID    string
+	X     int
+	Y     int
+	Size  int
+	Color string
+	Dead  bool
+}
 
 type Session struct {
-	Players []*players.Player
-	Targets []*targets.Target
+	ID      string
+	Players map[string]*Player
+	Targets map[string]*Target
+	events  chan SessionEvent
+	mu      sync.RWMutex
+}
+
+type Manager struct {
+	Sessions map[string]*Session
+}
+
+type SessionEvent struct {
+	Event string
+	Data  string
 }
--- click-trainer/internal/sessions/storage.go
+++ click-trainer/internal/sessions/storage.go
@@ -1,17 +1,82 @@
 package sessions
 
 import (
-	"clicktrainer/internal/players"
-	"clicktrainer/internal/targets"
+	"fmt"
+	"math/rand"
+	"sync"
+
+	gonanoid "github.com/matoous/go-nanoid/v2"
 )
 
 var (
-	sessions = make(map[string]*Session)
+	sessions      = make(map[string]*Session)
+	gameHeight    = 400
+	gameWidth     = 600
+	minTargetSize = 50
+	maxTargetSize = 100
 )
 
-func Get() Session {
-	return Session{
-		Players: players.GetList(),
-		Targets: targets.GetList(),
+func NewSession(id string) *Session {
+	session := &Session{
+		ID:      id,
+		Players: make(map[string]*Player),
+		Targets: make(map[string]*Target),
+		events:  make(chan SessionEvent),
+		mu:      sync.RWMutex{},
 	}
+	return session
+}
+
+func (s *Session) AddPlayer(id string, name string) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.Players[id] = &Player{ID: id, Name: name}
+	s.broadcast("addPlayer", id)
+}
+
+func (s *Session) RemovePlayer(id string) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	delete(s.Players, id)
+	s.broadcast("removePlayer", id)
+}
+
+func (s *Session) PlayersSnapshot() []*Player {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	list := make([]*Player, 0, len(s.Players))
+	for _, p := range s.Players {
+		list = append(list, p)
+	}
+	return list
+}
+
+func (s *Session) AddTarget() {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	if id, err := gonanoid.New(6); err != nil {
+		targetSize := rand.Intn(maxTargetSize-minTargetSize) + minTargetSize
+		s.Targets[id] = &Target{
+			ID:    id,
+			X:     rand.Intn(gameWidth - targetSize),
+			Y:     rand.Intn(gameHeight - targetSize),
+			Color: RandomColorHex(),
+			Size:  targetSize,
+		}
+	}
+
+}
+
+func (s *Session) broadcast(evt, data string) {
+	select {
+	case s.events <- SessionEvent{Event: evt, Data: data}:
+	default:
+	}
+}
+
+func RandomColorHex() string {
+	r := uint8(rand.Intn(248) + 4)
+	g := uint8(rand.Intn(248) + 4)
+	b := uint8(rand.Intn(248) + 4)
+	return fmt.Sprintf("#%02x%02x%02x", r, g, b)
 }
